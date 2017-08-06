60436c6f04d01dc71675e801aad43eca4aff9777
Wang Zhenxuan <nomorewzx@163.com>
2017-08-06 16:45:47 +0800

    Add summary for existed model and add another model with cross entropy as loss func.

--- cross_entropy_loss_pong.py
+++ cross_entropy_loss_pong.py
@@ -1,9 +1,5 @@
 """
-This model's loss function and initializer is built up by referring greydanus's implementation,
-see: https://gist.github.com/greydanus/5036f784eec2036252e1990da21eda18
-
-This model will take around 5300 episodes to compete with its opponent.
-And I trained this model on Mac Pro 15 Inch (16G RAM, Core i7) about 12 hours.
+Add summary for pong model, and change loss func to log prob.
 """
 import numpy as np
 import tensorflow as tf
@@ -30,41 +26,57 @@ DECAY = 0.99
 
 ACTION_SPACE = [STILL, MOVE_UP, MOVE_DOWN]
 
-CHECKPOINT_DIR = "saved_model/"
+CHECK_POINT_DIR = "saved_model_cross_entropy_loss/"
 
-SUMMARY_DIR = "summary/l2_loss/"
+SUMMARY_DIR = "summary/cross_entropy_loss/"
 
 graph = tf.Graph()
+
+
+def inference_model(input_statuses):
+
+    hidden_layer_output = tf.nn.relu(tf.matmul(input_statuses, hidden_layer_weights))
+    sampled_policy_logit = tf.matmul(hidden_layer_output, output_layer_weights)
+    action_probs = tf.nn.softmax(sampled_policy_logit)
+
+    return action_probs
+
+
+def build_loss_func(logits, rewards):
+    advantaged_cross_entropy = (-tf.multiply(labels, tf.log(logits))) * rewards
+    loss = tf.reduce_sum(advantaged_cross_entropy)
+
+    return loss
+
+
+def build_optimizer(loss):
+    optimizer = tf.train.RMSPropOptimizer(learning_rate=LEARNING_RATE, decay=DECAY)
+    gradients = optimizer.compute_gradients(loss=loss)
+    train_op = optimizer.apply_gradients(gradients, global_step=global_step)
+
+    return train_op
+
+
 with graph.as_default():
 
     global_step = tf.Variable(0, dtype=tf.int32, trainable=False, name='global_step')
 
     input_statuses = tf.placeholder(shape=[None, INPUT_DIM], dtype=tf.float32, name="input_status_by_episode")
     labels = tf.placeholder(shape=[None, ACTION_SPACE_SIZE], dtype=tf.float32, name="labels")
+    discounted_rewards = tf.placeholder(shape=[None, 1], dtype=tf.float32, name="discounted_rewards_by_episode")
+    rewards = tf.placeholder(shape=[None, 1], dtype= tf.float32, name="rewards_by_episode")
 
     hidden_layer_weights = tf.Variable(tf.truncated_normal([INPUT_DIM, HIDDEN_LAYER_COUNTS],mean=0.0, stddev= 1. / np.sqrt(INPUT_DIM), dtype=tf.float32), name="hidden_layer_weights")
     output_layer_weights = tf.Variable(tf.truncated_normal([HIDDEN_LAYER_COUNTS, ACTION_SPACE_SIZE], mean=0.0, stddev= 1. / np.sqrt(HIDDEN_LAYER_COUNTS), dtype=tf.float32), name="output_layer_weights")
 
-    hidden_layer_output = tf.nn.relu(tf.matmul(input_statuses, hidden_layer_weights))
+    action_probs = inference_model(input_statuses)
 
-    sampled_policy_logit = tf.matmul(hidden_layer_output, output_layer_weights)
 
-    action_probs = tf.nn.softmax(sampled_policy_logit)
+    logits = inference_model(input_statuses)
 
-    rewards = tf.placeholder(shape= [None, 1], dtype= tf.float32, name= "rewards_by_episode")
-    discounted_rewards = tf.placeholder(shape=[None, 1], dtype=tf.float32, name="discounted_rewards_by_episode")
+    loss = build_loss_func(logits, discounted_rewards)
 
-    hidden_layer_outputs = tf.nn.relu(tf.matmul(input_statuses, hidden_layer_weights))
-
-    logits = tf.matmul(hidden_layer_outputs, output_layer_weights)
-    softmax_logits = tf.nn.softmax(logits= logits)
-
-    loss = tf.nn.l2_loss(labels - softmax_logits)
-
-    optimizer = tf.train.RMSPropOptimizer(learning_rate=LEARNING_RATE, decay= DECAY)
-
-    gradients = optimizer.compute_gradients(loss= loss, grad_loss= discounted_rewards)
-    train_op = optimizer.apply_gradients(gradients, global_step= global_step)
+    train_op = build_optimizer(loss)
 
     loss_summary = tf.summary.scalar("loss", loss)
     reward_summary = tf.summary.scalar("reward", tf.reduce_sum(rewards))
@@ -72,12 +84,11 @@ with graph.as_default():
     summary_op = tf.summary.merge_all()
 
 with tf.Session(graph=graph) as session:
-
-    saver = tf.train.Saver(max_to_keep= 4)
     writer = tf.summary.FileWriter(SUMMARY_DIR, graph= session.graph)
 
+    saver = tf.train.Saver(max_to_keep= 4)
     tf.global_variables_initializer().run()
-    check_point = tf.train.get_checkpoint_state(os.path.dirname(CHECKPOINT_DIR + 'check_point'))
+    check_point = tf.train.get_checkpoint_state(os.path.dirname(CHECK_POINT_DIR + 'checkpoint'))
     if check_point and check_point.model_checkpoint_path:
         saver.restore(session, check_point.model_checkpoint_path)
     pong_env = gym.make("Pong-v0")
@@ -132,16 +143,15 @@ with tf.Session(graph=graph) as session:
             episode_labels = np.array(one_hot_action_list, dtype=np.float32)
             episode_statuses = np.array(input_status_list, dtype=np.float32)
 
-            feed_dict = {input_statuses: input_status_list, discounted_rewards: episode_discounted_rewards, labels: episode_labels, rewards: episode_rewards}
+            feed_dict = {input_statuses: input_status_list, rewards: episode_discounted_rewards, labels: episode_labels}
 
             _, loss_train, logits_train, global_step_train, summary_op_train = session.run([train_op, loss, logits, global_step, summary_op], feed_dict=feed_dict)
             print("Resetting env. episode reward total was %f . Loss is %f" % (reward_sum, loss_train))
             reward_sum = 0
-            saver.save(session, CHECKPOINT_DIR + 'pong_model', global_step= global_step_train)
+            saver.save(session, CHECK_POINT_DIR + 'pong_model', global_step= global_step_train)
             observation = pong_env.reset()  # reset env
             prev_status = None
             writer.add_summary(summary_op_train, global_step= global_step_train)
-
             input_status_list = []
             reward_list = []
             one_hot_action_list = []
